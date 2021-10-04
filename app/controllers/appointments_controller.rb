ebc1f2e921fd52950e0adbd26467ff73a3a30322
franpb14 <franperejonbarrios@gmail.com>
2021-10-04 19:14:42 +0200

    fix: I have changed the controller to avoid the error

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -15,16 +15,13 @@ class AppointmentsController < ApplicationController
   def create
     @appointment = Appointment.new(appointment_params)
     doctor_id = params[:appointment][:doctor_id]
+    @user_appointment = UserAppointment.new(user_id: current_user.id)
+    @doctor_appointment = UserAppointment.new(user_id: doctor_id)
+    @appointment.user_appointments << [@user_appointment, @doctor_appointment]
+
     if @appointment.save
-      @user_appointment = UserAppointment.new(user_id: current_user.id, appointment_id: @appointment.id)
-      @doctor_appointment = UserAppointment.new(user_id: doctor_id, appointment_id: @appointment.id)
-      if @user_appointment.save && @doctor_appointment.save
-        flash[:success] = 'Appointment successfully created'
-        redirect_to appointments_path
-      else
-        flash[:danger] = 'Something went wrong'
-        redirect_to new_appointment_path(doctor_id)
-      end
+      flash[:success] = 'Appointment successfully created'
+      redirect_to appointments_path
     else
       flash[:danger] = 'Something went wrong. Make sure you have fullfilled all the fields.'
       redirect_to new_appointment_path(doctor_id)
